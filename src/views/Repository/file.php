ee6ba4195b4a4392822cf79c67035c4d121db9ea
Christophe Hurpeau <christophe@hurpeau.com>
2013-04-18 13:58:01 +0200

    SyntaxHighlighter.highlight

--- src/views/Repository/file.php
+++ src/views/Repository/file.php
@@ -2,5 +2,5 @@
 {include _viewMenu.php}
 	{include _viewMenuFile.php}
 	<pre class="clear block1 brush:{$brush}">{$content}</pre>
-	<? HHtml::jsReady('SyntaxHighlighter.all()') ?>
+	<? HHtml::jsReady('SyntaxHighlighter.highlight()') ?>
 </div>
--- src/web/js/index.js
+++ src/web/js/index.js
@@ -1,9 +1,9 @@
-includeCore('libs/jquery-latest');
+includeCore('libs/jquery-1.9.1-dev');
+includeCore('libs/jquery-migrate-1.1.1');
 
 includeCore('springbok.base');
 includeCore('springbok.breadcrumbs');
 includeCore('springbok.ajax');
-includeCore('springbok.tinymce');
 
 includePlugin('users/_users');
 
